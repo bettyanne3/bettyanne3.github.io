243292b14a72ff9f87bd5d312520574e48ab11bf
bettyanne3 <bettyanne3@gmail.com>
2025-03-01 11:29:45 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const greeting = document.getElementById("greeting");
 greeting.style.textAlign = "center";
 greeting.style.marginTop = "50px";
+const username = window.prompt("What is your Name?", "Name");
+greeting.textContent = "Welcome to my Website, " + username + "!";
